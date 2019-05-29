4ce2865ef497851a45b02787d4e70105c3e8ba93
Cameron Cuff <dev.ctcuff@gmail.com>
2019-05-29 15:55:47 -0400

    Added a callback for on connect

--- filter_listener.py
+++ filter_listener.py
@@ -2,15 +2,20 @@ from tweepy import StreamListener
 
 
 class FilterListener(StreamListener):
-    def __init__(self, callback):
+    def __init__(self, status_callback, on_connect_callback):
         super().__init__()
-        self.callback = callback
+        self._status_callback = status_callback
+        self._on_connect_callback = on_connect_callback
 
     def on_status(self, status):
-        self.callback(status)
+        self._status_callback(status)
 
     def on_connect(self):
+        self._on_connect_callback()
         print('Stream connected')
 
+    def on_warning(self, notice):
+        print(f'On warning: {notice}')
+
     def on_error(self, status_code):
-        print(status_code)
+        print(f'Error: {status_code}')
